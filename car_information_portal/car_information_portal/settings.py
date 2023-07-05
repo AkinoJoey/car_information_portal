df0789a7db17c37b73f0da39ec6c8ab4158e64b9
AkinoJoey <gakiya213@gmail.com>
2023-07-05 19:52:24 +0900

    cssを修正

--- car_information_portal/car_information_portal/settings.py
+++ car_information_portal/car_information_portal/settings.py
@@ -38,11 +38,9 @@ INSTALLED_APPS = [
     'django.contrib.messages',
     'django.contrib.staticfiles',
     'search_cars',
-    'corsheaders',
 ]
 
 MIDDLEWARE = [
-    'corsheaders.middleware.CorsMiddleware',
     'django.middleware.security.SecurityMiddleware',
     'django.contrib.sessions.middleware.SessionMiddleware',
     'django.middleware.common.CommonMiddleware',
@@ -125,7 +123,3 @@ STATIC_URL = 'static/'
 
 DEFAULT_AUTO_FIELD = 'django.db.models.BigAutoField'
 
-CORS_ALLOWED_ORIGINS = [
-    'http://localhost:8000',
-    'https://www.carqueryapi.com',
-]
