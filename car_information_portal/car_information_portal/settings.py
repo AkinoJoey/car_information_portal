879674525a062cd4ffd3adca98c3a41e45c6fb9b
AkinoJoey <gakiya213@gmail.com>
2023-07-20 16:51:24 +0900

    change secret_key

--- car_information_portal/car_information_portal/settings.py
+++ car_information_portal/car_information_portal/settings.py
@@ -20,7 +20,10 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 # See https://docs.djangoproject.com/en/4.2/howto/deployment/checklist/
 
 # SECURITY WARNING: keep the secret key used in production secret!
-SECRET_KEY = 'django-insecure-=^_xtiaapeu+pk0efh4wb^&g+7ugk790ia&a7r&$1xso#mz@+^'
+try:
+    from .local_settings import *
+except ImportError:
+    pass
 
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = False
